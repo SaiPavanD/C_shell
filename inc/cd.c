e0bdac3a7a93ab83c98c86929aa91096d0c0911b
SaiPavanD <saipvn4@gmail.com>
2016-09-22 18:52:05 +0530

    Implemented cd function. Test it. Check readme.txt for details

---
+++ inc/cd.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define size 1024
+
+int cd(char* inp)
+{
+  char path[size];
+  if(inp[0]!='~') strcpy(path,inp);
+  else
+  {
+    if(inp[1]=='/')
+    {
+      strcpy(path,getenv("HOME"));
+      strcat(path,inp+2);
+    }
+    else if(inp[1]!='\0'){
+      strcpy(path,"/home/");
+      strcat(path,inp+1);
+    }
+    else{
+      strcpy(path,getenv("HOME"));
+    }
+  }
+  int status = chdir(path);
+  if(status!=0)
+  {
+    printf("Cannot change directory to : %s\n",inp );
+    return 0;
+  }
+  else return 1;
+}
+
+// int main(int argc, char *argv[]) {
+//   char cwd[size];
+//   cd(argv[1]);
+//   getcwd(cwd,sizeof(cwd));
+//   printf("cwd is %s\n",cwd );
+//   return 0;
+// }
---
+++ readme.txt
@@ -0,0 +1,11 @@
+cd.c is at inc/
+uncomment the main() code, compile it and run the executable similar to cd command.
+eg) ./a.out ~/Downloads
+
+Make note of any bugs.
+
+If pushing, don't add temporary files like .o
+
+Maintain a proper directory hierarchy.
+
+B)
